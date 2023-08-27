549298e1f341990f0c7520e31da4252d7ba98ca0
LTimur <ledenev-timur@ya.ru>
2023-08-27 13:16:57 +0500

    feat: add slider logic

--- index.js
+++ index.js
@@ -1,28 +1,87 @@
-const burgerOpenButton = document.querySelector('.burger-icon');
-const burgerCloseButton = document.querySelector('.burger-close');
-let burgerMenu = document.querySelector('.burger-menu');
+// Burger
 
-// Hide the burger menu initially
-burgerMenu.style.display = 'none';
+const burgerOpenButton = document.querySelector(".burger-icon");
+const burgerCloseButton = document.querySelector(".burger-close");
+let burgerMenu = document.querySelector(".burger-menu");
 
-// Function to open the burger menu
+burgerMenu.style.display = "none";
+
 function openBurgerMenu() {
-  burgerMenu.style.display = 'block';
-  burgerMenu.classList.remove('roll-up-animation');
-  burgerMenu.classList.add('roll-down-animation');
+  burgerMenu.style.display = "block";
+  burgerMenu.classList.remove("roll-up-animation");
+  burgerMenu.classList.add("roll-down-animation");
 }
 
-// Function to close the burger menu
 function closeBurgerMenu() {
-  burgerMenu.classList.remove('roll-down-animation');
-  burgerMenu.classList.add('roll-up-animation');
+  burgerMenu.classList.remove("roll-down-animation");
+  burgerMenu.classList.add("roll-up-animation");
   setTimeout(() => {
-    burgerMenu.style.display = 'none';
-  }, 500); // Change the value if you want to adjust the animation duration
+    burgerMenu.style.display = "none";
+  }, 500);
 }
 
-// Add a click event listener to the open button
-burgerOpenButton.addEventListener('click', openBurgerMenu);
+burgerOpenButton.addEventListener("click", openBurgerMenu);
+burgerCloseButton.addEventListener("click", closeBurgerMenu);
 
-// Add a click event listener to the close button
-burgerCloseButton.addEventListener('click', closeBurgerMenu);
+// Slider
+const sliderLeftButton = document.querySelector(".slider-leftbutton");
+const sliderRightButton = document.querySelector(".slider-rightbutton");
+const sliderItems = document.querySelector(".slider-line");
+const sliderWrapper = document.querySelector(".slider-wrapper");
+const dots = document.querySelectorAll(".slider-button");
+const width = 450;
+let offset = 0;
+
+// Slider dots
+dots.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    dots.forEach((button) => button.classList.remove('slider-button--active'));
+    button.classList.add('slider-button--active');
+    
+    let translateX = 0
+    if(window.getComputedStyle(sliderWrapper).width === '1400px'){
+      translateX = index * -475;
+    }
+    else{
+      translateX = index * -450;
+    }
+
+    sliderItems.style.transform = `translateX(${translateX}px)`;
+  });
+});
+
+function slideLeftSlider() {
+  if (offset > 0) {
+    offset -= width;
+    const activeDotIndex = offset / width;
+    
+    dots.forEach((button, index) => {
+      if (index === activeDotIndex) {
+        button.classList.add('slider-button--active');
+      } else {
+        button.classList.remove('slider-button--active');
+      }
+    });
+  }
+  sliderItems.style.transform = `translateX(-${offset}px)`;
+}
+
+function slideRightSlider() {
+  const maxOffset = (dots.length - 1) * width;
+  if (offset < maxOffset) {
+    offset += width;
+    const activeDotIndex = offset / width;
+    
+    dots.forEach((button, index) => {
+      if (index === activeDotIndex) {
+        button.classList.add('slider-button--active');
+      } else {
+        button.classList.remove('slider-button--active');
+      }
+    });
+  }
+  sliderItems.style.transform = `translateX(-${offset}px)`;
+}
+
+sliderLeftButton.addEventListener("click", slideLeftSlider);
+sliderRightButton.addEventListener("click", slideRightSlider);
